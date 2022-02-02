c833bdbd45e70a0665ccd020954b0d9c68a73e72
Admin <Admin@DESKTOP-0H54U56>
2022-02-02 21:29:01 +0530

    First commit

---
+++ FirstRepo/src/com/calculator/addition/Calculator.java
@@ -0,0 +1,14 @@
+package com.calculator.addition;
+
+public class Calculator {
+	
+	public static void main(String[] args) {
+		
+		int a= 10;
+		int b= 20;
+		int c= a+b;
+		System.out.println(c);
+		
+	}
+
+}
