38884d21326b7dbee0eae778482c791c306e2eaf
Surya78922 <98870755+Surya78922@users.noreply.github.com>
2022-02-03 12:06:31 +0530

    Update Calculator.java

--- FirstRepo/src/com/calculator/addition/Calculator.java
+++ FirstRepo/src/com/calculator/addition/Calculator.java
@@ -4,12 +4,9 @@ public class Calculator {
 	
 	public static void main(String[] args) {
 		
-		int a= 20;
-		int b= 20;
+		
 		int d = a*b;
-		int c= a+b;
-		System.out.println(c);
-		System.out.println(d);
+	       System.out.println(d);
 	}
 
 }
